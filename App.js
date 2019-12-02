84a0cd36439f719e102ec92bfc21106cdae09702
dipanshu <dipanshu.garg@girnarsoft.com>
2019-12-02 12:16:42 +0530

    Todo list

--- App.js
+++ App.js
@@ -1,47 +1,31 @@
 import React, {Component} from 'react';
-import {StyleSheet,} from 'react-native';
-import CounterApp from "./src/CounterApp";
-import {createStore} from 'redux';
+import TodoApp from "./src/TodoApp";
+import store from "./src/store"
 import {Provider} from 'react-redux';
-
-const initialState = {
-    counter: 0
-};
-
-const reducer = (state = initialState, action) => {
-
-    switch (action.type) {
-        case 'INCREASE_COUNTER':
-            return {counter: state.counter + 1};
-        case 'DECREASE_COUNTER':
-            return {counter: state.counter - 1};
-    }
-    return state;
-};
-
-const store = createStore(reducer);
+import {StyleSheet} from "react-native";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
             <Provider store={store}>
-                <CounterApp/>
+                <TodoApp/>
             </Provider>
         );
     }
 
-};
+}
+
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center', flex: 1, justifyContent: 'center'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 
 });
+
 
 export default App;
